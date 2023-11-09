a5a8b9d31de1c9554e68eae5cf7881bbb96186b2
Jason Cavanaugh <jascav418@gmail.com>
2023-11-08 19:14:58 -0500

    Beginngin of file reorg

---
+++ src/pages/Expenses.tsx
@@ -0,0 +1,203 @@
+import { Expense } from "@prisma/client";
+import * as RadixModal from "@radix-ui/react-dialog";
+import { useState } from "react";
+import Modal from "src/components/Modal";
+import { Spinner } from "src/components/Spinner";
+import { api } from "src/utils/api";
+import { cents_to_dollars_display } from "src/utils/centsToDollarDisplay";
+import { cn } from "src/utils/cn";
+import { BaseColor } from "src/utils/colors";
+import { TW_COLORS_MP } from "src/utils/tailwindColorsMp";
+import { ExpenseDataByDay, use_expenses } from "src/utils/useExpenses";
+
+export function Expenses() {
+  const expense_data_query = use_expenses();
+  return (
+    <ul className="flex flex-col gap-4">
+      {expense_data_query.status === "loading" && (
+        <div className="flex h-[95vh] items-center justify-center">
+          <Spinner className="h-16 w-16 border-4 border-solid border-pikachu dark:border-rengar dark:border-rengar_light lg:border-8" />
+        </div>
+      )}
+      {expense_data_query.status === "error" && (
+        <div className="flex h-[95vh] items-center justify-center">
+          <h1 className="text-white">
+            Uh oh, there was a problem loading your expenses.
+          </h1>
+        </div>
+      )}
+      {expense_data_query.status === "success" &&
+        expense_data_query.data.expenses.length === 0 && (
+          <div className="flex h-[95vh] items-center justify-center">
+            <h1 className="text-white">
+              Click the '+' button to add a new expense.
+            </h1>
+          </div>
+        )}
+      {expense_data_query.status === "success" &&
+        expense_data_query.data.expenses.length > 0 && (
+          <ChronologicalExpenseList
+            expenses_by_day={expense_data_query.data.expenses}
+            category_id_to_color={expense_data_query.data.category_id_to_color}
+            category_id_to_name={expense_data_query.data.category_id_to_name}
+          />
+        )}
+    </ul>
+  );
+}
+
+function ChronologicalExpenseList({
+  expenses_by_day,
+  category_id_to_name,
+  category_id_to_color,
+}: {
+  expenses_by_day: ExpenseDataByDay[];
+  category_id_to_color: Map<string, BaseColor>;
+  category_id_to_name: Map<string, string>;
+}) {
+  let output = [];
+  for (const dwe of expenses_by_day) {
+    output.push(
+      <li key={dwe.id} className="px-3 py-4">
+        <h1 className="inline rounded-lg bg-squirtle px-2 py-1 font-bold text-white dark:bg-rengar md:p-2">
+          {dwe.date_display}
+        </h1>
+        <div className="h-4" />
+        <ul className="flex flex-col gap-3 rounded-lg bg-pikachu p-4 shadow-sm dark:bg-leblanc dark:shadow-sm dark:shadow-leblanc">
+          <ExpenseListForDay
+            category_id_to_expenses_for_day={dwe.category_id_to_expenses}
+            category_id_to_color={category_id_to_color}
+            category_id_to_name={category_id_to_name}
+          />
+          <li className="flex justify-between">
+            <p className="font-semibold text-squirtle dark:text-rengar">
+              Total:{" "}
+            </p>
+            <p className="font-semibold text-squirtle dark:text-rengar">
+              {cents_to_dollars_display(dwe.total_for_day)}
+            </p>
+          </li>
+        </ul>
+      </li>
+    );
+  }
+  return <>{output}</>;
+}
+
+function ExpenseListForDay({
+  category_id_to_expenses_for_day,
+  category_id_to_color,
+  category_id_to_name,
+}: {
+  category_id_to_expenses_for_day: Map<string, Expense[]>;
+  category_id_to_color: Map<string, BaseColor>;
+  category_id_to_name: Map<string, string>;
+}) {
+  let output = [];
+  for (const category_id of category_id_to_expenses_for_day.keys()) {
+    const expense_list = category_id_to_expenses_for_day.get(category_id)!;
+    const sum_of_expenses = expense_list.reduce((acc, e) => e.amount + acc, 0);
+    const category_color = category_id_to_color.get(category_id)!;
+    const category_name = category_id_to_name.get(category_id);
+    output.push(
+      <li key={category_id}>
+        <div className="flex justify-between">
+          <h2
+            className={`flex items-center rounded-lg ${TW_COLORS_MP["bg"][category_color][200]} px-2 text-sm font-bold md:py-1 md:text-base ${TW_COLORS_MP["text"][category_color][700]}`}
+          >
+            {category_name}
+          </h2>
+          <p className="font-semibold text-squirtle dark:text-rengar">
+            {cents_to_dollars_display(sum_of_expenses)}
+          </p>
+        </div>
+        <ul className="flex flex-wrap gap-1 py-2">
+          {expense_list.map((expense, i) => {
+            return (
+              <ExpenseButton
+                expense={expense}
+                category_color={category_color}
+              />
+            );
+          })}
+        </ul>
+      </li>
+    );
+  }
+  return <>{output}</>;
+}
+
+function ExpenseButton({
+  expense,
+  category_color,
+}: {
+  expense: Expense;
+  category_color: BaseColor;
+}) {
+  const [is_modal_open, set_is_modal_open] = useState(false);
+  const expense_data_query = use_expenses();
+  const api_utils = api.useContext();
+  const delete_expense = api.router.delete_expense.useMutation({
+    onSuccess: () => {
+      expense_data_query.invalidate_queries();
+      set_is_modal_open(false); //TODO: Have to figure out how to close the modal once the new data comes in
+    },
+    onError: () => {
+      alert("error");
+    },
+  });
+  return (
+    <Modal
+      open={is_modal_open}
+      trigger={
+        <li
+          key={expense.id}
+          className={`rounded-full ${TW_COLORS_MP["bg"][category_color][500]} px-2 text-white hover:cursor-pointer`}
+          onClick={() => set_is_modal_open(true)}
+        >
+          {cents_to_dollars_display(expense.amount)}
+        </li>
+      }
+      className={cn(
+        "fixed left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col rounded bg-pikachu dark:bg-leblanc",
+        "border-t-8 border-t-red-500 px-5 py-3 md:rounded-lg lg:top-1/2 lg:w-[30rem] lg:px-8 lg:py-6"
+      )}
+    >
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          delete_expense.mutate({ id: expense.id });
+        }}
+      >
+        <RadixModal.Title className="whitespace-nowrap text-3xl font-bold text-slate-700 dark:text-white">
+          Delete Expense
+        </RadixModal.Title>
+        <div className="h-1 lg:h-4" />
+        <div className="flex w-full flex-col gap-4">
+          Are you sure you wish to delete this expense?
+        </div>
+        <div className="h-8" />
+        <div className="flex justify-center gap-5">
+          <button
+            className="h-[2rem] w-[4.5rem] rounded-full bg-slate-500 text-xs font-semibold text-white hover:brightness-110 lg:h-[3rem] lg:w-[7rem] lg:text-base lg:font-bold"
+            type="button"
+            onClick={() => set_is_modal_open(false)}
+          >
+            Cancel
+          </button>
+          <button
+            className={cn(
+              "flex w-[4.5rem] items-center justify-center rounded-full bg-red-500 text-xs font-semibold text-white lg:h-[3rem] lg:w-[7rem] lg:text-base lg:font-bold"
+            )}
+            type="submit"
+          >
+            {delete_expense.status === "loading" && (
+              <Spinner className="h-4 w-4 border-2 border-solid border-white lg:h-5 lg:w-5" />
+            )}
+            {delete_expense.status !== "loading" && "Delete"}
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
---
+++ src/pages/Visualize.tsx
@@ -0,0 +1,264 @@
+import { format, subDays } from "date-fns";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { DateRange } from "react-day-picker";
+import { Spinner } from "src/components/Spinner";
+import { Ugh } from "src/server/api/routers/router";
+import { api } from "src/utils/api";
+import { cents_to_dollars_display } from "src/utils/centsToDollarDisplay";
+import { cn } from "src/utils/cn";
+import { BaseColor } from "src/utils/colors";
+import { TW_COLORS_TO_HEX_MP } from "src/utils/tailwindColorsToHexMp";
+import { VictoryPie, VictoryTooltip } from "victory";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "src/components/shadcn/Popover";
+import { Button } from "src/components/shadcn/Button";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "src/components/shadcn/Calendar";
+
+export function Visualize() {
+  //const expense_data_query = use_expenses();
+  const { theme } = useTheme();
+  const week_ago_date = subDays(new Date(), 7);
+  const today_date = new Date();
+  const [date, set_date] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  }); //Default to the past week
+
+  const expense_data_query = api.router.get_expenses_over_date_range.useQuery({
+    from_date: {
+      day: date && date.from ? date.from.getDate() : week_ago_date.getDate(),
+      month_idx:
+        date && date.from ? date.from?.getMonth() : week_ago_date.getMonth(),
+      year:
+        date && date.from
+          ? date.from?.getFullYear()
+          : week_ago_date.getFullYear(),
+    },
+    to_date: {
+      day: date && date.to ? date.to.getDate() : today_date.getDate(),
+      month_idx: date && date.to ? date.to.getMonth() : today_date.getMonth(),
+      year: date && date.to ? date.to.getFullYear() : today_date.getFullYear(),
+    },
+  });
+
+  console.log("expense_data_query", expense_data_query.data);
+
+  return (
+    <div className="flex flex-col gap-4">
+      {expense_data_query.status === "loading" && (
+        <div className="flex h-[95vh] items-center justify-center">
+          <Spinner className="h-16 w-16 border-4 border-solid border-pikachu dark:border-rengar dark:border-rengar_light lg:border-8" />
+        </div>
+      )}
+      {expense_data_query.status === "error" && (
+        <div className="flex h-[95vh] items-center justify-center">
+          <h1 className="text-white">
+            Uh oh, there was a problem loading your expenses.
+          </h1>
+        </div>
+      )}
+      {expense_data_query.status === "success" && (
+        <>
+          <DatePickerWithRange date={date} set_date={set_date} />
+          <div className="flex h-[85vh] flex-col items-center justify-center bg-pikachu dark:bg-leblanc md:flex-row">
+            <div className="w-1/2">
+              <VictoryPie
+                labels={[]}
+                padAngle={5}
+                innerRadius={70}
+                labelComponent={
+                  <VictoryTooltip
+                    // flyoutWidth={65}
+                    // flyoutHeight={35}
+                    cornerRadius={5}
+                    // center={{
+                    //   x: 0,
+                    //   y: 0
+                    // }}
+                    flyoutPadding={{ top: 5, bottom: 5, left: 10, right: 10 }}
+                    pointerLength={0}
+                    flyoutStyle={{
+                      strokeWidth: 0,
+                      fill: ({ datum }) => {
+                        const color = datum.color.join("");
+                        return TW_COLORS_TO_HEX_MP[color as BaseColor]["200"];
+                      },
+                      boxShadow: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+                    }}
+                    // @ts-ignore
+                    style={{
+                      fill: ({ datum }: { datum: any }) => {
+                        const color = datum.color.join("");
+                        return TW_COLORS_TO_HEX_MP[color as BaseColor][
+                          "700"
+                        ] as string;
+                      },
+                      fontSize: 10,
+                      fontWeight: 600,
+                      textAnchor: "middle",
+                    }}
+                  />
+                }
+                // style={{
+                //   labels: {
+                //     fontSize: 8,
+                //     fill: theme === "dark" ? "white" : "black",
+                //   },
+                // }}
+                radius={100}
+                colorScale={get_colors(
+                  get_data_intermediate(expense_data_query.data)
+                )}
+                animate={{
+                  animationWhitelist: ["style", "data", "size"], // Try removing "size"
+                  onExit: {
+                    duration: 500,
+                    before: () => ({ opacity: 0.3, _y: 0 }),
+                  },
+                  onEnter: {
+                    duration: 500,
+                    before: () => ({ opacity: 0.3, _y: 0 }),
+                    after: (datum) => ({ opacity: 1, _y: datum._y }),
+                  },
+                }}
+                // labelComponent={<VictoryLabel className="border bg-red-700 px-4" />}
+                // data={[
+                //   { y: 75, label: "jason" },
+                //   { y: 5, label: "maureen" },
+                //   { y: 20, label: "jeremy" },
+                // ]}
+                data={get_pie_chart_data(
+                  get_data_intermediate(expense_data_query.data)
+                )}
+              />
+            </div>
+            <div className="flex w-[50%] flex-col gap-3 border">
+              <div className="flex items-center gap-2 rounded p-4 font-bold">
+                <div className="h-4 w-4 rounded-full bg-pink-500" />
+                <p>Groceries</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+function get_colors(input: IntResp) {
+  return input.props.map((d) => TW_COLORS_TO_HEX_MP[d.color]["500"]);
+}
+
+//For now...
+type IntResp = {
+  global_total: number;
+  props: {
+    name: string;
+    color: BaseColor;
+    total: number;
+  }[];
+};
+function get_data_intermediate(days_and_ec: Ugh): IntResp {
+  const out: Record<string, { name: string; color: BaseColor; total: number }> =
+    {};
+  const category_id_to_color: Record<string, BaseColor> = {};
+  for (const ec of days_and_ec.expense_categories) {
+    category_id_to_color[ec.id] = ec.color;
+  }
+  const category_id_to_name: Record<string, string> = {};
+  for (const ec of days_and_ec.expense_categories) {
+    category_id_to_name[ec.id] = ec.name;
+  }
+
+  let global_total = 0;
+  for (const d of days_and_ec.days) {
+    for (const e of d.expenses) {
+      if (!out[e.category_id]) {
+        out[e.category_id] = {
+          name: category_id_to_name[e.category_id]!,
+          color: category_id_to_color[e.category_id]!,
+          total: 0,
+        };
+      }
+      out[e.category_id]!.total += e.amount;
+      global_total += e.amount;
+    }
+  }
+  // Object.values(out);
+  return { global_total, props: Object.values(out) };
+}
+
+function get_pie_chart_data(input: IntResp) {
+  const out = input.props.map((d) => {
+    const c = d.color;
+    return {
+      y: d.total / input.global_total,
+      label: `${d.name} - ${cents_to_dollars_display(d.total)} (${Math.floor(
+        (d.total / input.global_total) * 100
+      ).toLocaleString()}%)`,
+      color: d.color.split(""), //Because Victory is the weirdest fucking library
+    };
+  });
+  return out;
+}
+
+function DatePickerWithRange({
+  className,
+  date,
+  set_date,
+}: {
+  className?: string;
+  date: DateRange | undefined;
+  set_date: (new_date: DateRange | undefined) => void;
+}) {
+  // const [date, setDate] = useState<DateRange | undefined>({
+  //   from: subDays(new Date(), 7),
+  //   to: new Date()
+  // });//Default to the past week
+
+  return (
+    <div className={cn("grid gap-2")}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant="outline"
+            className={cn(
+              "w-[300px] justify-start border border-slate-400 text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from && date.to && (
+              <>
+                {format(date.from, "LLL dd, y")} -{" "}
+                {format(date.to, "LLL dd, y")}
+              </>
+            )}
+            {date?.from && !date.to && format(date.from, "LLL dd, y")}
+            {!date?.from && !date?.to && <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className="w-auto border-none bg-white p-0 dark:bg-leblanc"
+          align="start"
+        >
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={set_date}
+            numberOfMonths={2}
+            showOutsideDays={false}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
