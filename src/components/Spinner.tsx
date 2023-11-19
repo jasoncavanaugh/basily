5a394c463e399efa1010d489e36f143440f1fceb
Jason Cavanaugh <jascav418@gmail.com>
2023-11-18 22:15:04 -0500

    Fix spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -4,8 +4,8 @@ export function Spinner({ className = "" }: { className?: string }) {
   return (
     <div
       className={cn(
-        "animate-spin rounded-full border-t-transparent dark:border-t-transparent",
-        className
+        className,
+        "animate-spin rounded-full border-t-transparent dark:border-t-transparent"
       )}
     />
   );
