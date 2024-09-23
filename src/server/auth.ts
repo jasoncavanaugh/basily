b35974ea69fd489d68b2531f99865dd6bc0d8d83
Jason Cavanaugh <jascav418@gmail.com>
2024-09-23 13:43:08 -0400

    Add todo

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -56,6 +56,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      //TODO: Fix this!
       checks: ["none"],
     }),
     /**
